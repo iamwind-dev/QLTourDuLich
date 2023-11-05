f3302effd17370922a24a13f9602123121116216
iamWinD <129095904+iamwind-dev@users.noreply.github.com>
2023-11-06 02:56:50 +0700

    first commit

---
+++ .history/js/script_20231106021026.js
@@ -0,0 +1,33 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () { 
+    header.classList.toggle("sticky", window.scrollY>0);
+})
+
+let menu=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    navbar.classList.remove("open");
+};
+
+const sections = document.querySelectorAll("[data-section]");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+sections.forEach((el) => observer
